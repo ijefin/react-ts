90df543ed73081910e3c1dbb3f6dba6d708917f9
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-25 20:03:34 -0300

    im back

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { render } from "react-dom";
 
 export const Events = () => {
   const handleLog = (e) => {
     console.log(e);
   };
 
+  const rendering = (x) => {
+    if (x) {
+      return <h1>Sim, sou verdadeiro xd</h1>;
+    } else {
+      return <h1>Negativo capitão broxa, sou falso!</h1>;
+    }
+  };
+
   return (
     <>
       <h1>lets Event!</h1>
       <button onClick={handleLog}>Click me!</button>
+      <button onClick={() => console.log("Também funciono :3")}>
+        Click me!
+      </button>
+
+      {rendering(true)}
+      {rendering(false)}
     </>
   );
 };
