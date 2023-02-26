4c12a19bfbf300fe4eddf76b1921edbda140e3dd
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-26 10:37:09 -0300

    listing with index

--- src/components/ManageListData.jsx
+++ src/components/ManageListData.jsx
@@ -2,26 +2,33 @@ import React, { useState } from "react";
 
 const ManageListData = () => {
   const [list] = useState(["primeiro", "segundo", "terceiro"]);
-  const [show, setShow] = useState(false);
-
-  const handleShowList = () => {
-    setShow(!show);
-  };
+  const [obj] = useState([
+    { id: 1, name: "Jeff", job: "Developer" },
+    { id: 2, name: "Carlo", job: "Developer" },
+    { id: 3, name: "Pedro", job: "Developer" },
+    { id: 4, name: "Jona", job: "Developer" },
+    { id: 5, name: "Miguel", job: "Developer" },
+    { id: 6, name: "Alfred", job: "Developer" },
+  ]);
 
   return (
     <>
       <h1>Teste</h1>
-      <button onClick={handleShowList}>Mostrar lista!</button>
       <div>
-        {show ? (
+        {
           <ul>
-            {list.map((item) => (
-              <li>{item}</li>
+            {list.map((item, index) => (
+              <li key={index}>{item}</li>
             ))}
           </ul>
-        ) : (
-          <></>
-        )}
+        }
+      </div>
+      <div>
+        {obj.map((data) => (
+          <li key={data.id}>
+            {data.name}, {data.job}
+          </li>
+        ))}
       </div>
     </>
   );
