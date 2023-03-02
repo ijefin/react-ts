5572e2bdda6f696d033e296987299020c30edb6f
ijefin <jeffersonmecmiler@hotmail.com>
2023-03-02 16:22:05 -0300

    inputs with label as a parent

--- React Forms/src/components/Forms/MyForm.tsx
+++ React Forms/src/components/Forms/MyForm.tsx
@@ -5,19 +5,19 @@ export const MyForm = () => {
 
   const [data, setData] = useState("")
 
-  const handleData = (e: any) => {
-    setData(e.target.value)
-  }
+  // const handleData = (e: any) => {
+  //   setData(e.target.value)
+  // }
 
   return (
     <>
       <form >
         <label>
           <span>Email:</span>
-          <input onChange={handleData} type="email" />
+          <input onChange={(e) => setData(e.target.value)} type="email" />
           <h1>{data}</h1>
           <span>Password:</span>
-          <input type="password" value={data} />
+          <input type="password" onChange={(e) => setData(e.target.value)} />
         </label>
         <input type="submit" value="Send" />
       </form>
