dca44270a3d84a0f2bb314f3d4f4c5952dcb3ce8
ijefin <jeffersonmecmiler@hotmail.com>
2023-03-05 14:07:20 -0300

    created game component

--- secret-word/src/App.tsx
+++ secret-word/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css"
 import { StartScreen } from "./componentes/StartScreen/StartScreen"
 import { useCallback, useState } from "react"
 import { words } from "./data/words.js"
+import { Game } from "./componentes/Game/Game"
+import GameOver from "./componentes/GameOver/GameOver"
 
 export const App = () => {
 
@@ -17,12 +19,19 @@ export const App = () => {
   ]
 
   const [gameStage, setgameStage] = useState(stages[0].name)
+  const [word, setWord] = useState(words)
+
+  console.log(word)
+
+  const handlePlay = () => {
+    setgameStage(stages[1].name)
+  }
 
   return (
     <div className="app">
-      {gameStage === "menu" && < StartScreen />}
-      {/* {gameStage === "playing" && < StartScreen />}
-      {gameStage === "end" && < StartScreen />} */}
+      {gameStage === "menu" && < StartScreen set={handlePlay} />}
+      {gameStage === "playing" && < Game />}
+      {gameStage === "end" && < GameOver />}
     </div>
   )
 }
--- secret-word/src/componentes/StartScreen/StartScreen.tsx
+++ secret-word/src/componentes/StartScreen/StartScreen.tsx
@@ -1,13 +1,13 @@
 import "./StartScreen.css"
 
-export const StartScreen = () => {
+export const StartScreen = ({ set }: any) => {
     return (
         <>
             <div className="main-title">
                 <h1>Secret Word!</h1>
                 <p>Excelente didática para fortalecer seu vocabulário e também a memória.</p>
                 <p>Clique no botão para jogar!</p>
-                <button className="play">Iniciar</button>
+                <button onClick={set} className="play">Iniciar</button>
             </div>
         </>
     )
