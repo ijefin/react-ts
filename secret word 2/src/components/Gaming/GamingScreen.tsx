02fe58442e36c08c49ac0bd142072e1ed6ec5272
ijefin <jeffersonmecmiler@hotmail.com>
2023-03-20 11:54:58 -0300

    addded string manipulation

--- secret word 2/src/components/Gaming/GamingScreen.tsx
+++ secret word 2/src/components/Gaming/GamingScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { SetStateAction, useState } from "react"
 import { Screens } from "../../interfaces/Screens"
 import { Button } from "../Button/Button"
 import "./GamingScreen.css"
@@ -7,17 +7,25 @@ export const GamingScreen = ({ func, tip, secretWord }: Screens) => {
 
   const [inputed, setInputed] = useState("")
   const [inputedList, setInputedList] = useState(Array<string>)
+  const [alreadyPlayedLetters, setAlreadyPlayedLetters]: any = useState([])
 
   const handleSaveInputed = (e: any) => {
     e.preventDefault()
     if (secretWord?.includes(inputed)) {
       setInputedList([...inputedList, inputed.toLowerCase()])
+    } else {
+      setAlreadyPlayedLetters([...alreadyPlayedLetters, inputed])
     }
     setInputed("")
   }
-  const handleInputed = (e: any) => {
 
+  const handleInputed = (e: any) => {
     setInputed(e.target.value)
+  }
+
+  const handleAlredyInputed = () => {
+    const alredyInputed = inputedList.find((letter) => console.log(letter))
+    return
   }
 
   const handleShowOrNot: any = (letter: string, index: number) => {
@@ -31,27 +39,26 @@ export const GamingScreen = ({ func, tip, secretWord }: Screens) => {
   console.log(secretWord)
 
   return (
-    <>
-      <div>
-        <h1>
-          Adivinhe a palavra!
-        </h1>
-        <h2>Dica: {tip}</h2>
-        <div className="letters-container">
-          {
-            secretWord?.map((letter: string, index: number) => (
-              handleShowOrNot(letter, index)
-            ))
-          }
-        </div>
-        <label htmlFor="">
-          <input value={inputed} onChange={handleInputed} type="text" placeholder="Digite uma letra" />
-          <button onClick={handleSaveInputed}>OK</button>
-        </label>
-        <div>
-          <Button handleFunc={func} text="Iniciar" />
-        </div>
+    <div>
+      <h1>
+        Adivinhe a palavra!
+      </h1>
+      <h2>Dica: {tip}</h2>
+      <div className="letters-container">
+        {
+          secretWord?.map((letter: string, index: number) => (
+            handleShowOrNot(letter, index)
+          ))
+        }
       </div>
-    </>
+      <label htmlFor="">
+        <input value={inputed} onChange={handleInputed} type="text" maxLength={1} placeholder="Digite uma letra" />
+        <button onClick={handleSaveInputed}>OK</button>
+      </label>
+      <div>
+        <Button handleFunc={func} text="Iniciar" />
+      </div>
+      <h5>{alreadyPlayedLetters.map((letter: string) => letter + ", ")} </h5>
+    </div>
   )
 }
