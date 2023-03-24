4d3342c44ddaa523c0c94a2593456bc2cdf1e2f8
ijefin <jeffersonmecmiler@hotmail.com>
2023-03-24 16:09:13 -0300

    posting method

--- json-server/src/App.tsx
+++ json-server/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { EventHandler, useEffect, useState } from "react";
 import "./App.css";
 import { Card } from "./components/Card";
 
@@ -13,6 +13,8 @@ export const App = () => {
   const url = "http://localhost:3000/products";
 
   const [products, setProducts] = useState(Array<data>);
+  const [name, setproductName] = useState(String)
+  const [price, setproductPrice] = useState(Number)
 
   useEffect(() => {
     const fetchData = async (): Promise<Array<data>> => {
@@ -26,11 +28,44 @@ export const App = () => {
 
   }, []);
 
-  console.log(products)
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
+
+    const newProduct = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct)
+    })
+
+    console.log(res)
+  }
 
   return (
-    <div className="App">
-      {products.map((prod) => <Card key={prod.id} title={prod.name} price={prod.price} />)}
-    </div>
+    <>
+      <div className="App">
+        {products.map((prod) => <Card key={prod.id} title={prod.name} price={prod.price} />)}
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="productName">
+            Nome:
+          </label>
+          <input type="text" name="productName" value={name} onChange={(e) => setproductName(e.target.value)} />
+          <label htmlFor="productPrice">
+            Preço:
+          </label>
+          <input type="number" id="productPrice" onChange={(e) => setproductPrice(parseInt(e.target.value))} />
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+    </>
+
   );
 };
